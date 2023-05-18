885822ac5ee9f912cabf631e1a5306789be287b5
simigami <lgoljm@gmail.com>
2023-05-19 00:45:32 +0900

    hm.rb

--- scripts/HackMate/makedb.rb
+++ scripts/HackMate/makedb.rb
@@ -17,11 +17,11 @@ db_params = {
 
 table_params = "create table hm_profiles(
 	profile_id SERIAL PRIMARY KEY,
-    profile_name varchar(200) NOT NULL,
+  profile_name varchar(200) NOT NULL,
 	target_system_name varchar(100) NOT NULL,
 	ipv4 inet NOT NULL,
 	ipv6 inet NULL,
-    mac_address MACADDR NULL,
+  mac_address MACADDR NULL,
 	port integer NULL,
 	url varchar(200) NULL,
 	db_type varchar(100) NULL
@@ -47,15 +47,15 @@ rescue PG::Error => e
 
 ensure
   connection.close if connection
-  
+
 end
 
 def create_table(db_name, db_params, table_name, table_params)
     begin
       connection = PG::Connection.new(db_params)
-  
+
       result = connection.exec("SELECT 1 FROM pg_database WHERE datname='#{db_name}'")
-  
+
       if result.ntuples == 0
         puts 'No DB, Creating DB'
         connection.exec("CREATE DATABASE #{db_name}")
@@ -70,9 +70,9 @@ def create_table(db_name, db_params, table_name, table_params)
         puts 'Switched to "hackmate" database.'
 
       end
-  
+
       result = connection.exec("SELECT EXISTS (SELECT FROM information_schema.tables WHERE table_name = '#{table_name}')")
-  
+
       if result.getvalue(0, 0) == 'f'
         puts 'No Table, Creating Table'
         connection.exec(table_params)
@@ -83,11 +83,11 @@ def create_table(db_name, db_params, table_name, table_params)
         #   puts row['column_name']
         # end
       end
-  
+
     rescue PG::Error => e
       puts "Error: #{e.message}"
     ensure
       connection.close if connection
     end
 end
-#create_table(db_name, db_params, table_name, table_params)
+create_table(db_name, db_params, table_name, table_params)
--- scripts/HackMate/runNmap.rb
+++ scripts/HackMate/runNmap.rb
@@ -26,7 +26,7 @@ table_params = "create table hm_nmap_result (
   ipv6 inet NULL,
   mac_address MACADDR NULL,
   port integer ARRAY NULL,
-  port_description varchar(30) ARRAY NULL, 
+  port_description varchar(30) ARRAY NULL,
   version_names varchar(100) ARRAY NULL,
   OS_Guessing varchar(30) NULL
 )"
@@ -36,27 +36,31 @@ command_params = {
   taget_profile_name: "Test_To_Metasploitable2",
   target: "192.168.0.117",
   nmap_options: "-sV -T4 -O -oN",
-  log_location: "/home/user/Desktop/Capstone/HackMate/nmap_logs",
+  log_location: "/home/user/Desktop/Everything_Related_To_Git/Projects/Metasploit_Fork/hackmate/nmap_logs",
   auth: 0666
 }
 
 def run_nmap(command_params, db_name, db_params, table_name, table_params)
   file_name = create_file(command_params[:log_location], command_params[:auth])
+
   log_file_path = "#{command_params[:log_location]}/#{file_name}"
+
   command = "#{command_params[:cmd]} #{command_params[:nmap_options]} #{log_file_path} #{command_params[:target]}"
-  
+
+  puts command_params
+
   execute_command(command)
-  
+
   log = File.read(log_file_path)
-  
+
   ip_address = log[/Nmap scan report for (\S+)/, 1]
   mac_address = log[/MAC Address: (\S+)/, 1]
   ports = log.scan(/^\s*(\d+)\/\w+\s+/).flatten
   service_names = log.scan(/^\s*\d+\/\w+\s+\w+\s+(\S+)/).flatten
   version_names = log.scan(/\d+\/\w+\s+\w+\s+\w+\s+((?:\S+ )*\S*)$/).flatten.map { |str| str.empty? ? "NULL" : "'#{str}'" }
 
-  puts version_names
-  
+  #puts version_names
+
   result = {
     profile_name: command_params[:taget_profile_name],
     ports: ports,
@@ -65,7 +69,7 @@ def run_nmap(command_params, db_name, db_params, table_name, table_params)
     ip_address: ip_address,
     mac_address: mac_address
   }
-  
+
   data_params = {
     profile_name: result[:profile_name],
     ipv4: result[:ip_address],
@@ -76,11 +80,11 @@ def run_nmap(command_params, db_name, db_params, table_name, table_params)
     version_names: result[:version_names],
     OS_Guessing: nil
   }
-  
+
   create_table(db_name, db_params, table_name, table_params)
-  
+
   insert_query = "INSERT INTO #{table_name} (profile_name, ipv4, ipv6, mac_address, port, port_description, version_names, OS_Guessing) VALUES ($1, $2, $3, $4, $5::integer[], $6::varchar[], $7::varchar[], $8)"
-  
+
   insert_data_into_nmap_table(db_params, data_params, insert_query)
 end
 
